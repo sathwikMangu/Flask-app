ece042353339ffe52898c399147f713e52f34bf9
sathwikMangu <sathwikmangu8@gmail.com>
2022-05-06 18:25:23 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -48,5 +48,5 @@ def home():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0',port=8080)
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=False,host='0.0.0.0')
     #app.run(debug=True)
