73eb0e1933e21e2bfb0f9aebf609a61c3989cfc7
sathwikMangu <sathwikmangu8@gmail.com>
2022-05-07 07:45:27 +0530

    trying with port 8080

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def home():
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=5000)
-    # app.run(host='0.0.0.0',port=8080)
+    app.run(debug = False, host='0.0.0.0',port=8080)
     #app.run(debug=False,host='0.0.0.0')
-    app.run(debug=True)
+    #app.run(debug=True)
